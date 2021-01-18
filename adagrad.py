ab196b828c4f13cfbcf5c0dce34d936c8d837971
AnahVeronica <anahveronica98@gmail.com>
2021-01-18 19:58:13 +0530

    Adagrad included
    Signed-off-by: AnahVeronica <anahveronica98@gmail.com>

--- adagrad.py
+++ adagrad.py
@@ -2,21 +2,14 @@ import numpy as np
 from sklearn.utils import shuffle
 
 #Data points
-'''
-Generates random 
-x values
-'''
 x = np.linspace(0,5, 500)
 
 #True Function
-'''
-Creates a functions of f(x) = y
-'''
 bo = 2
 b1 = 3.14
 y = bo + b1*x
 
-def shuffled_data(x): #Shuffles the data for gradient descent
+def shuffled_data(x):
     x = shuffle(x)
     y = bo + b1*x
     return x, y
@@ -26,16 +19,22 @@ def loss_function(bo, b1, y, x):
     squared_error = (y - (bo + b1*x))**2
     return np.mean(squared_error)
 
-def dervative(x, y, bo, b1): #Computes the gradient
+#Derivative of the loss function with respect to the weights bo and b1
+def dervative(x, y, bo, b1):
     bo_derv = 2*(-y + bo + b1*x)
     b1_derv = 2*x*(-y + bo + b1*x)
     return bo_derv, b1_derv
 
-def gradient_descent(X, Y, bo, b1, eta=0.0001, tolerance=0.001, epochs = 1, beta = 0.9, batch_size = 16):
+def square(list): #To return square of sum of derivatives
+    if list != []:
+        return sum([i ** 2 for i in list])
+    else:
+        return 0
+
+
+def gradient_descent(X, Y, bo, b1, eta=0.0001, tolerance=0.0001, epochs = 1, epsilon = 0.01, batch_size = 16):
     prev_bo = 0
     prev_b1 = 0
-    vdw = 0
-    vdb = 0
 
     t = 0
 
@@ -58,11 +57,11 @@ def gradient_descent(X, Y, bo, b1, eta=0.0001, tolerance=0.001, epochs = 1, beta
         dbo = np.mean(dbo)
         db1 = np.mean(db1)
 
-        vdw = (beta * vdw) + (1 - beta) * db1
-        vdb = (beta * vdb) + (1 - beta) * dbo
+        eta_delta = eta / np.sqrt(square(b1_values) + epsilon)
 
-        bo -= eta*vdb
-        b1 -= eta*vdw
+
+        bo -= eta_delta*dbo
+        b1 -= eta_delta*db1
 
         print("At iteration no: ", t, " the loss of batch ", loss_function(bo, b1, Y_batch, X_batch), " absolute loss is ", loss_function(bo, b1, Y, X))
 
@@ -77,6 +76,6 @@ def gradient_descent(X, Y, bo, b1, eta=0.0001, tolerance=0.001, epochs = 1, beta
 
     return bo, b1
 
-bo_final, b1_final = gradient_descent(x, y, 0.1, 0.1, epochs= 200, batch_size= 64)
+bo_final, b1_final = gradient_descent(x, y, np.random.randn(), np.random.randn(), epochs= 200, batch_size= 64)
 
 print("The final weights are bo = ", np.round(bo_final, 3), " b1 = ", np.round(b1_final, 3))
