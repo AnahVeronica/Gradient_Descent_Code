bc7feba53f3c3add2f8237f72865de8874f9fc5c
AnahVeronica <anahveronica98@gmail.com>
2021-01-18 18:38:18 +0530

    Momentum included
    Signed-off-by: AnahVeronica <anahveronica98@gmail.com>

--- mini_batch_with_momentum.py
+++ mini_batch_with_momentum.py
@@ -2,14 +2,21 @@ import numpy as np
 from sklearn.utils import shuffle
 
 #Data points
+'''
+Generates random 
+x values
+'''
 x = np.linspace(0,5, 500)
 
 #True Function
+'''
+Creates a functions of f(x) = y
+'''
 bo = 2
 b1 = 3.14
 y = bo + b1*x
 
-def shuffled_data(x):
+def shuffled_data(x): #Shuffles the data for gradient descent
     x = shuffle(x)
     y = bo + b1*x
     return x, y
@@ -19,15 +26,16 @@ def loss_function(bo, b1, y, x):
     squared_error = (y - (bo + b1*x))**2
     return np.mean(squared_error)
 
-#Derivative of the loss function with respect to the weights bo and b1
-def dervative(x, y, bo, b1):
+def dervative(x, y, bo, b1): #Computes the gradient
     bo_derv = 2*(-y + bo + b1*x)
     b1_derv = 2*x*(-y + bo + b1*x)
     return bo_derv, b1_derv
 
-def gradient_descent(X, Y, bo, b1, eta=0.0001, tolerance=0.0001, epochs = 1, batch_size = 16):
+def gradient_descent(X, Y, bo, b1, eta=0.0001, tolerance=0.0001, epochs = 1, beta = 0.9, batch_size = 16):
     prev_bo = 0
     prev_b1 = 0
+    vdw = 0
+    vdb = 0
 
     t = 0
 
@@ -50,8 +58,11 @@ def gradient_descent(X, Y, bo, b1, eta=0.0001, tolerance=0.0001, epochs = 1, bat
         dbo = np.mean(dbo)
         db1 = np.mean(db1)
 
-        bo -= eta*dbo
-        b1 -= eta*db1
+        vdw = (beta * vdw) + (1 - beta) * db1
+        vdb = (beta * vdb) + (1 - beta) * dbo
+
+        bo -= eta*vdb
+        b1 -= eta*vdw
 
         print("At iteration no: ", t, " the loss of batch ", loss_function(bo, b1, Y_batch, X_batch), " absolute loss is ", loss_function(bo, b1, Y, X))
 
